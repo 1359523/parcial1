3a6e17d7965e1ff98f58af645f64d5399f0dc406
1359523 <armoratay@correo.url.edu.gt>
2025-08-13 21:02:17 -0600

    Se sube el proyecto

---
+++ src/components/FormularioLogin.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function FormularioLogin({Login}){    
+    
+    //Habilitar el boton hasta que se hallan llenado la casillas
+    const [codigo, setCodigo] = useState(''); //Se le asigna al codigo ''
+    const [usuario, setUsuario] = useState(''); //Le asgina al usuario ''
+    const [password, setPassword] = useState(''); //Le asigna a la contrasenia ''
+    
+
+    const habilitarBoton = usuario.trim() !== '' && password.trim() !== '' && codigo.trim() !== ''; //trim se utiliza para ignorar los espacios
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid && Login) {
+      Login({ codigoCliente, usuario, pswd });
+    }
+    };
+    //Mostrar las casillas de usuario y de password
+    return (
+        
+
+        <div>
+            <input type="text" placeholder="Codigo" value ={codigo} onChange={(e) => setCodigo(e.target.value)} />
+            <input type="text" placeholder="Usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+            <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button disabled={!habilitarBoton}>Iniciar Sesión</button>
+        </div>
+    );
+
+
+    /*CODIGO MINIMO PARA QUE EL PRIMER TEST PASE
+
+    //Mostrar las casillas de codigo, usuario y contrasenia
+    return (
+        <div>
+            <input type="text" placeholder="Codigo" />
+            <input type="text" placeholder="Usuario" />
+            <input type="password" placeholder="Contraseña"/>
+            <button disabled>Login</button>
+        </div>
+    ); */
+} 
---
+++ src/tests/FormularioLogin.test.jsx
@@ -0,0 +1,44 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import FormularioLogin from '../components/FormularioLogin';
+
+test('el boton Login esta deshabilitado inicialmente', () => {
+    render(<FormularioLogin/>);
+    const botonLogin = screen.getByRole('button', { name: /Iniciar Sesión/i});
+    expect(botonLogin).toBeDisabled();
+})
+
+//Test para ver que el boton este habilitado
+test('El boton se habilita cuando codigo, usuario y contrasena tienen contenido', () => {
+    render(<FormularioLogin />);
+    const inputCodigo = screen.getByPlaceholderText(/codigo/i);
+    const inputUsuario = screen.getByPlaceholderText(/usuario/i);
+    const inputPassword = screen.getByPlaceholderText(/contraseña/i);
+    const botonLogin = screen.getByRole('button', {name: /Iniciar Sesión/i});
+
+    fireEvent.change(inputCodigo, {target: {value: '1359523'}});
+    fireEvent.change(inputUsuario, {target: {value: 'miusuario'}});
+    fireEvent.change(inputPassword, {target: {value: 'password'}});
+
+    expect(botonLogin).toBeEnabled();
+})
+
+test("Al hacer clic en el botón se llama a Login con los datos correctos", () => {
+    const mockLogin = jest.fn();
+    render(<FormularioLogin Login={mockLogin} />);
+    const codigoInput = screen.getByPlaceholderText(/codigo/i);
+    const usuarioInput = screen.getByPlaceholderText(/usuario/i);
+    const pswdInput = screen.getByPlaceholderText(/contraseña/i);
+    const button = screen.getByRole("button", { name: /iniciar sesión/i });
+
+    fireEvent.change(codigoInput, { target: { value: "123" } });
+    fireEvent.change(usuarioInput, { target: { value: "Juan" } });
+    fireEvent.change(pswdInput, { target: { value: "1234" } });
+    fireEvent.click(button);
+
+    expect(mockLogin).toHaveBeenCalledWith({
+      codigoCliente: "123",
+      usuario: "Juan",
+      pswd: "1234",
+    });
+});
